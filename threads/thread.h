55eca8a50d5a14d4d057985aabea74747967a645
Myung Kyungbeck <kbmyung@naver.com>
2014-04-16 02:21:46 +0900

    Add time_to_wakeup attribute in the thread struct.

--- threads/thread.h
+++ threads/thread.h
@@ -100,6 +100,7 @@ struct thread
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
+    int64_t time_to_wakeup; // Wake up time.
   };
 
 /* If false (default), use round-robin scheduler.
